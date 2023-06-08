7dabfb5351fad5d10137b4bc69bc6d897f9838da
Atlanmail <tiep123@trieuvan.com>
2023-06-08 14:41:03 -0700

    Working on Parseroll

--- src/Components/DiceRoller.js
+++ src/Components/DiceRoller.js
@@ -1,30 +1,101 @@
 const roll = (diceSize) => {
+
+    //diceSize = parseInt(diceSize)
     return Math.floor(Math.random() * diceSize + 1);
 
 } 
 
+
+function containsOnlyAllowedValues(str) {
+    const regexPattern = new RegExp(`^[0-9 \+\-d]*$`);
+    return regexPattern.test(str);
+}
+
+function containsCharD(str) {
+    const regexPattern = new RegExp(`/d/`);
+    return regexPattern.test(str);
+} 
 /**
  * 
  * @param {string} input 
  * 
- * checks if string contains 
+ * checks if string is valid dice roller
  */
 
 export function isValidDicestring (input) {
-    input.replace(/\s/g,'');
-    let result = input.split(/[+-]/);
-    console.log(result);
+    input = input.replaceAll(/\s/g,'');
+    //console.log(input)
+    const result = input.split(/[+-]/);
+    //console.log(result);
+
+    for (const value of result) {
+        if (containsOnlyAllowedValues(value) == false) { /// checks if it contains the allowed values only
+            //console.log(value + " 1")
+            return false;
+        }
+        var containsD = false;
+        for (var i = 0 ; i < value.length ; i++) { /// checks if it has multiple ds
+            if (value.charAt(i) == 'd') {
+                if (containsD == true) {
+                    //console.log(value + " 2")
+                    return false;
+                }
+                containsD = true;
+            }
+
+        }
+
+        if (value.charAt(value.length - 1) == 'd') {
+            return false;
+        }
+    }
 
     return true;
 }
 
 /**
  * 
- * @param {string} input containing only d4, +, etc 
+ * @param {string} input containing only d4, +, integers 
  * 
- * takes in a string such as d4+10 
+ * takes in a string such as d4+10 then spits out an output that is all the dice rolled.
  */
 
-const parseRoll = (input) => {
+export function parseRoll(input) {
+    if (isValidDicestring(input) == false) {
+        return -1;
+      }
     
+      var output = 0;
+      input = input.replaceAll(/\s/g, '');
+    
+      const result = input.split(/[+-]/);
+      console.log(result);
+    
+      for (const value of result) {
+        var toAdd = 0;
+    
+        if (containsCharD(value) == false) {
+          console.log("Doesn't contain D");
+          output += parseInt(value);
+          continue;
+        }
+    
+        const splitValue = value.split(/[d]/);
+        console.log(splitValue);
+    
+        if (splitValue.length === 1) {
+          // No number of dice rolls specified, default to 1
+          toAdd += roll(splitValue[0]);
+        } else {
+          for (var i = 0; i < splitValue[0]; i++) {
+            toAdd += roll(splitValue[1]);
+          }
+        }
+    
+        output += toAdd;
+        console.log(output);
+      }
+    
+      return output;
+
 }
--- src/Components/DiceRoller.test.js
+++ src/Components/DiceRoller.test.js
@@ -1,5 +1,56 @@
 import * as DiceRoller from './DiceRoller.js';
 
-test('is d4 + 2 valid', () => {
+/**test('is d4 + 2 valid', () => {
     expect(DiceRoller.isValidDicestring("d4 + 2")).toBe(true);
 }); 
+
+test('is 44d4 + 2 valid', () => {
+    expect(DiceRoller.isValidDicestring( "44d4 + 2")).toBe(true);
+}); 
+test('is 44d + 2 valid', () => {
+    expect(DiceRoller.isValidDicestring("44d + 2")).toBe(false);
+}); 
+
+test('is 44dd2 + 2 valid', () => {
+    expect(DiceRoller.isValidDicestring("44dd2 + 2")).toBe(false);
+});**/ 
+
+test ('d6', () => {
+    const result = DiceRoller.parseRoll("d6");
+
+    expect(result).toBeDefined();
+    expect(result).toBeGreaterThanOrEqual(1);
+    expect(result).toBeLessThanOrEqual(6);
+    expect(result% 1).toBe(0);
+
+});
+/** 
+test ('2d6', () => {
+    const result = DiceRoller.parseRoll("2d6");
+
+    expect(result).toBeDefined();
+    expect(result).toBeGreaterThanOrEqual(2);
+    expect(result).toBeLessThanOrEqual(12);
+    expect(result% 1).toBe(0);
+
+});*/
+
+test ('d6 + 4', () => {
+    const result = DiceRoller.parseRoll("d6+4");
+
+    expect(result).toBeDefined();
+    expect(result).toBeGreaterThanOrEqual(5);
+    expect(result).toBeLessThanOrEqual(10);
+    expect(result% 1).toBe(0);
+
+});
+
+/**test ('5', () => {
+    const result = DiceRoller.parseRoll("5");
+
+    expect(result).toBeDefined();
+    expect(result).toBeGreaterThanOrEqual(5);
+    expect(result).toBeLessThanOrEqual(5);
+    expect(result% 1).toBe(0);
+
+});**/
